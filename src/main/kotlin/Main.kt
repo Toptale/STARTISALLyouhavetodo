3b3cacfa9926e325d69d3a00db015da8307d4f6b
Toptale <122173384+Toptale@users.noreply.github.com>
2022-12-29 18:02:18 +0000

    tests:

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,3 +3,16 @@ import data.models.Block
 import data.models.Ledger
 import data.models.Transaction
 import data.models.Wallet
+
+fun main(args: Array<String>) {
+    println("+-------------------------------------------------------------------------------------------------------------------------------------------------------+")
+    println("|\t########################################################## Launching N-Blockchain App... ##########################################################\t|")
+
+    println("|\tCreating the ledger...\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|")
+    val ledger = Ledger()
+    println("|\tLedger created :D\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|")
+    println("|\tGenesis block is :\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|")
+    println(Block.GENESIS.toString())
+
+    println("|\tCreating wallets :\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|")
+    val wallets: ArrayList<Wallet> = arrayListOf()
