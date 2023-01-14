687171d2871aa51f03b8d380526870986fb417d7
Toptale <122173384+Toptale@users.noreply.github.com>
2023-01-14 05:39:08 +0000

    1.3.9

---
+++ src/main/kotlin/config/crypto/constants.kt
@@ -0,0 +1,9 @@
+
+package config.crypto
+
+import java.security.MessageDigest
+import java.security.Signature
+
+// Message digest
+val MD: MessageDigest = MessageDigest.getInstance("SHA-256")
+// Signature
