60e046348b21f490c5f16f1d66d8ed0434fc730e
Toptale <122173384+Toptale@users.noreply.github.com>
2023-01-14 08:14:31 +0000

    added release notes

--- src/main/kotlin/config/crypto/constants.kt
+++ src/main/kotlin/config/crypto/constants.kt
@@ -7,3 +7,11 @@ import java.security.Signature
 // Message digest
 val MD: MessageDigest = MessageDigest.getInstance("SHA-256")
 // Signature
+val S = Signature.getInstance("SHA256withECDSA")
+
+val HASH_SIZE: Int = when(MD.algorithm) {
+    "SHA-256" -> 64
+    else -> 32
+}
+
+const val HASH_DIFFICULTY = "a0"
