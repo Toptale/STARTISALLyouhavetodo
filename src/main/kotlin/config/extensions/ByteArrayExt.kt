721e354ec4b97204871dea8d6946f5498d11253b
Toptale <122173384+Toptale@users.noreply.github.com>
2023-01-29 11:28:36 +0000

    Linux 3.10.99

--- src/main/kotlin/config/extensions/ByteArrayExt.kt
+++ src/main/kotlin/config/extensions/ByteArrayExt.kt
@@ -3,3 +3,12 @@ package config.extensions
 
 import java.security.KeyFactory
 import java.security.PublicKey
+import java.security.spec.X509EncodedKeySpec
+
+fun ByteArray.toHexString(): String = joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
+
+fun ByteArray.toPublicKey(): PublicKey =
+    KeyFactory.getInstance("EC")
+        .generatePublic(
+            X509EncodedKeySpec(this)
+        )
