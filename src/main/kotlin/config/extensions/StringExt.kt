ee496ac9ad24e98dd8de77b3aecaccdb7105ea84
Toptale <122173384+Toptale@users.noreply.github.com>
2023-02-04 22:09:26 +0000

    Update books

--- src/main/kotlin/config/extensions/StringExt.kt
+++ src/main/kotlin/config/extensions/StringExt.kt
@@ -8,3 +8,12 @@ fun String.toHashString(): String {
     MD.update(toByteArray())
     return BigInteger(1, MD.digest()).toString(16)
 }
+
+
+fun String.fromHexString(): ByteArray =
+    if (length % 2 != 0)
+        byteArrayOf()
+    else
+        chunked(2)
+            .map { it.toInt(16).toByte() }
+            .toByteArray()
