83a82b363c0a42a0809df745a56a36316d292980
Toptale <122173384+Toptale@users.noreply.github.com>
2023-02-04 05:25:08 +0000

    Update netinst.sh

--- src/main/kotlin/config/extensions/StringExt.kt
+++ src/main/kotlin/config/extensions/StringExt.kt
@@ -1,2 +1,10 @@
 
 package config.extensions
+
+import config.crypto.MD
+import java.math.BigInteger
+
+fun String.toHashString(): String {
+    MD.update(toByteArray())
+    return BigInteger(1, MD.digest()).toString(16)
+}
