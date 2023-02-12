6e37cd491e77e727b0a6a13f5167b02f74a980ee
Toptale <122173384+Toptale@users.noreply.github.com>
2023-02-12 08:48:35 +0000

    Variable rename

--- src/main/kotlin/data/models/Block.kt
+++ src/main/kotlin/data/models/Block.kt
@@ -8,3 +8,16 @@ import config.crypto.exceptions.WrongDifficultySizeException
 import config.extensions.toHashString
 import config.extensions.toHexString
 import data.dto.BlockDto
+import data.dto.TransactionDto
+import data.types.Hashable
+import data.types.Miner
+import data.types.TransactionArray
+import data.types.Verifiable
+import java.util.*
+import kotlin.jvm.Throws
+
+class Block : Hashable,
+    Miner,
+    Verifiable {
+
+    val transactions: TransactionArray = TransactionArray()
