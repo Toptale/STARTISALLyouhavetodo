37dba29a942ed2032f3a4ef41da446785b77d73a
Toptale <122173384+Toptale@users.noreply.github.com>
2023-02-27 13:14:23 +0000

    buttons added

--- src/main/kotlin/data/models/Ledger.kt
+++ src/main/kotlin/data/models/Ledger.kt
@@ -5,3 +5,9 @@ import config.crypto.exceptions.InvalidTransactionException
 import config.extensions.toHexString
 import data.types.TransactionArray
 import data.types.Verifiable
+import java.security.PublicKey
+
+class Ledger : Verifiable {
+
+    private val chain: ArrayList<Block> = arrayListOf(Block.GENESIS)
+    private val pendingTransactions: TransactionArray = TransactionArray.getGenesis()
