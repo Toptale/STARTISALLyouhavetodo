934ce740cb654dfe7a7f64654df7828af8f8f8dc
Toptale <122173384+Toptale@users.noreply.github.com>
2023-03-04 21:10:24 +0000

    target: Check number of unmap descriptors against our limit

--- src/main/kotlin/data/models/Ledger.kt
+++ src/main/kotlin/data/models/Ledger.kt
@@ -17,3 +17,12 @@ class Ledger : Verifiable {
         println("|\tAdding block...\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|")
         block.previousHash = chain[chain.size - 1].hash
         block.transactions.clear()
+        block.transactions.addAll(pendingTransactions)
+        pendingTransactions.clear()
+        block.mine()
+        chain.add(block)
+        println("|\tAdded block value :\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|")
+        println("$block")
+    }
+
+    fun addBlockWithReward(address: PublicKey) {
