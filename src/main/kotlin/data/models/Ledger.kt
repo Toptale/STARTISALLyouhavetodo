8244a41f24d3078fc1312ffae8a01f6164d32ecd
Toptale <122173384+Toptale@users.noreply.github.com>
2023-03-04 20:18:31 +0000

    staging: r8712u: Set device type to wlan

--- src/main/kotlin/data/models/Ledger.kt
+++ src/main/kotlin/data/models/Ledger.kt
@@ -11,3 +11,9 @@ class Ledger : Verifiable {
 
     private val chain: ArrayList<Block> = arrayListOf(Block.GENESIS)
     private val pendingTransactions: TransactionArray = TransactionArray.getGenesis()
+
+    fun addBlock() {
+        val block = Block()
+        println("|\tAdding block...\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|")
+        block.previousHash = chain[chain.size - 1].hash
+        block.transactions.clear()
