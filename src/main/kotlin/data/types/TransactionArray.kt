6854dfe50341ce61e4ca62280b0c578a647f1701
Toptale <122173384+Toptale@users.noreply.github.com>
2023-03-26 23:08:51 +0000

    Preparing release 1.4

--- src/main/kotlin/data/types/TransactionArray.kt
+++ src/main/kotlin/data/types/TransactionArray.kt
@@ -2,3 +2,9 @@
 package data.types
 
 import data.models.Transaction
+
+class TransactionArray : ArrayList<Transaction>() {
+
+    override fun toString(): String {
+        var result = ""
+        forEach { transaction ->
