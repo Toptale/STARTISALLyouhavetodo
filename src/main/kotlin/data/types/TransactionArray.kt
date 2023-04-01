65c447adcf917b54f0e387ba89cd649ef86d14dd
Toptale <122173384+Toptale@users.noreply.github.com>
2023-04-01 19:55:21 +0000

    msm: kgsl: Keep the GPU from going into a slumber after a touch event

--- src/main/kotlin/data/types/TransactionArray.kt
+++ src/main/kotlin/data/types/TransactionArray.kt
@@ -8,3 +8,13 @@ class TransactionArray : ArrayList<Transaction>() {
     override fun toString(): String {
         var result = ""
         forEach { transaction ->
+            result += transaction.toString()
+        }
+        return result
+    }
+
+    companion object {
+        private val GENESIS = TransactionArray()
+
+        fun getGenesis(): TransactionArray =
+            if(GENESIS.isEmpty()) {
